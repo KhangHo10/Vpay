9002fa4a5e21498746870d0afaae2727e7627a4f
HaruCrisp <haru.hori001@gmail.com>
2025-09-27 03:44:52 -0400

    Add/update App.jsx React component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Voice Payment MVP</h1>
-      <button>Start Voice Command</button>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
